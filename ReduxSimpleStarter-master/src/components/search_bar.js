b841a537a56911a0a475add63a16c809d5e9f9b6
chaminn <chmk90@gmail.com>
2017-12-31 20:08:27 +0800

    YouTube Search Application using React JS Completion

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -1,14 +1,5 @@
 import React, {Component} from 'react';
 
-// functional component
-// Other is Class based component
-// const SearchBar = () => {
-//   return <input />;
-// };
-
-
-//similar to
-// const Component = React.Component
 class SearchBar extends Component { //React.Component
 
 /**
@@ -23,37 +14,18 @@ Class based component states are track as follows
     // we are going to make the state of this app the value that user type
   }
 
-  //need to return JSX
-  //render method
   render(){
-    //return JSX;
-    //every class must have a render functional
-    //return <input onChange={this.onInputChange} />;
-  //  return <input onChange={(event) => {console.log(event.target.value);} } />;
-  //  return <input onChange={event => console.log(event.target.value); } />;
-
       return (
-        <div>
+        <div className="search-bar">
         <input
         value = {this.state.term}
-        onChange={event => this.setState({term : event.target.value}) } />
-
+        onChange={event => this.onInputChange(event.target.value) } />
         </div>
       );
-      /**
-        Controlled Component value is set by the state
-        input field value is set to state.
-        so can't change:
-      **/
-      //setState to change states
   };
-
-// when input change run this code
-//pass the funcion to element that need to moniter this event
-  onInputChange(event){
-    console.log(event.target.value);
+  onInputChange(term){
+    this.setState({term});//set state
+    this.props.onSearchTermChange(term);//call back from index.js
   }
 }
-// instances by new SearchBar()
-
 export default SearchBar;
--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,33 +1,55 @@
 /**
  * Created by chaminn on 12/30/2017.
  */
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch  from 'youtube-api-search';
 import SearchBar from './components/search_bar'; //fileName = search_bar : give file references: actual relative path
-//Create new component
-//Produce some HTML
-// const App = function(){
-//   return <div>Hi CK !!!
-//   <ol>
-//     <li>Mango</li>
-//     <li>Watermalon</li>
-//     <li>Grapes</li>
-//   </ol>
-//   </div>;
-// }
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA_QiWareUZzMMqGnYkUzxiwI8rQhJbZ2E';
 
-const App = () => {
-  return <div>
-<SearchBar/>
 
+class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+                    videos : [],
+                    selectedVideo: null
+                   };
+      this.videoSearch('surfboards');
+  }
 
-  </div>;
+  //define serarch callback
+  //this passed to SearchBar under the property onSearchTermChange
+  videoSearch(term){
+    YTSearch({key: API_KEY , term: term }, (videos) =>{
+        //this.setState({videos: videos}); //key and value same ==> condense
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      }
+    );
+  }
+
+  render(){
+    //throttle user input
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 1000)
+    return( <div>
+  <SearchBar
+      onSearchTermChange={term => this.videoSearch(term)}
+  />
+  <VideoDetail video={this.state.selectedVideo} />
+  <VideoList
+    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+    videos={this.state.videos}/>
+    </div>
+  );
+  }
+
 }
 
-// once write component, put into DOM //
-//take this component 's generated HTML' and put on DOM
-// to make an instance we have to wrap it inside JSX components
-// When putting add a target location, 2nd argument
 ReactDOM.render(<App/>, document.querySelector('.container'));
